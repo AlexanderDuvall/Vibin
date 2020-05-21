f45b1bc229751c71c94cc688b7dc4a0457f6b7b6
Alexander Duvall <ultimateweapon9@gmail.com>
2020-05-21 14:19:09 -0500

    Added four image post. Fixed imageExpander formats. Fixed image post formats.

--- app/javascript/components/UserSignUp/UserHeader.jsx
+++ app/javascript/components/UserSignUp/UserHeader.jsx
@@ -3,38 +3,38 @@ import insert from  '../../../assets/images/insert.png'
 import  avatar from '../../../assets/images/default-avatar.jpg'
 class UserHeader extends React.Component{
 
-    selectImage(type){
-        document.getElementById(type).trigger('click');
+    selectImage = (type) => {
+        let t = type + "Input";
+        console.log(t);
+        document.getElementById(t).click();
     }
 
-    handleChange(image){
-        let im = image + "Image";
-        document.getElementById(im).src = window.URL.createObjectURL(this.files[0]);
-    }
-
-
+    handleChange = (image) => {
+        let im = image.substring(0, image.length-5);
+        console.log(im);
+        }
 
     render() {
         return(
         <div className="Header" id="header">
             <div className="LeftHeader" id="leftHeader">
-                <img src={insert} className="LeftHeaderImage" id="leftHeaderImage" onClick={this.selectImage("firstHeader")}/>
+                <img src={insert} className="LeftHeaderImage" id="leftHeaderImage" onClick={() => { this.selectImage("firstHeader") }} />
                 <div className="Badge LeftBadge" id="badge1">
                     Banner Color
                 </div>
             </div>
             <div className="Avatar" id="avatar">
-                <img src={avatar} style={{width: 100, height: 100}} id="avatarImage" onClick={this.selectImage("avatarUpload")}/>
+                <img src={avatar} style={{width: 100, height: 100}} id="avatarImage" onClick={() => {this.selectImage("avatar")}}/>
             </div>
             <div className="RightHeader" id="rightHeader">
-                <img src={insert} className="RightHeaderImage" id="rightHeaderImage" onClick={this.selectImage("secondHeader")}/>
+                <img src={insert} className="RightHeaderImage" id="rightHeaderImage" onClick={() => {this.selectImage("secondHeader")}}/>
                 <div className="Badge RightBadge" id="badge2">
                     Banner Text Color
                 </div>
             </div>
-            <input hidden className="photo_upload" type="file" name="user[firstHeader]" id="firstHeader" onChange={this.handleChange()}/>
-            <input hidden className="photo_upload" type="file" name="user[secondHeader]" id="secondHeader" onChange={this.handleChange()}/>
-            <input hidden className="photo_upload" type="file" name="user[avatar]" id="avatarUpload" onChange={this.handleChange()}/>
+            <input hidden className="photo_upload" type="file" name="user[firstHeader]" id="firstHeaderInput" onChange={() => {this.handleChange(this.id)}}/>
+            <input hidden className="photo_upload" type="file" name="user[secondHeader]" id="secondHeaderInput" onChange={() => {this.handleChange(this.id)}}/>
+            <input hidden className="photo_upload" type="file" name="user[avatar]" id="avatarInput" onChange={() => {this.handleChange(this.id)}}/>
         </div>
         );
     }
